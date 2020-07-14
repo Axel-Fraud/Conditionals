33c1948bc2d86546fd06ddd55c77cb9fbe6a3d98
Axel Fraud <frauax1@gmail.com>
2020-07-14 20:29:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+class Main {
+  public static void main(String[] args) {
+    
+   // System.out.println(35%40);
+
+for(int i = 1; i<=50; i++){
+
+
+ if(0 == i%15){
+System.out.println("Fizzbuzz");
+}
+else if(0 == i%5){
+System.out.println("Buzz");
+}
+else if(0== i%3){
+  System.out.println("Fizz");
+}
+
+
+else{
+  System.out.println(i);
+}
+
+}
+int x = 1000;
+for( int i = 1; i<=x; i++){
+  if(0 == x%i){
+  System.out.println(i);
+  }
+}
+
+
+//create variable and save string in it print the word in the variable in reverse
+//dynamic
+
+String input = boby;
+int x = input.length() 
+for( int i = 1; i<=x; i++){
+System.out.println(i);
+}
+*/
+String input = "boby"; 
+for(int i = input.length() ; i >= 1; i--) {
+    System.out.println(input.charAt(i-1));
+}
+  }
+}
